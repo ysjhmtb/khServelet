a8f62e1d89f280fa323aa28879186adb41d9de31
YSJ <ysjhmtb@gmail.com>
2018-05-28 18:13:49 +0900

    28

---
+++ khWorkspace/thirdWebProject/src/com/kh/java/member/controller/LoginServlet.java
@@ -0,0 +1,38 @@
+package com.kh.java.member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login.dd")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 전송값에 한글이 있을 경우, 한글 처리는 이제 할 필요가 없다. 
+		// EncodingFilter가 이미 설정되어 있다.
+		
+		String id = request.getParameter("id");
+		
+		
+		
+	}
+
+}
