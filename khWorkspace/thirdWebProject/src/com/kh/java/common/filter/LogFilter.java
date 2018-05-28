88468a993d7348d48095f970430655da57879511
YSJ <ysjhmtb@gmail.com>
2018-05-28 17:57:25 +0900

    28

---
+++ khWorkspace/thirdWebProject/src/com/kh/java/common/filter/LogFilter.java
@@ -0,0 +1,53 @@
+package com.kh.java.common.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Servlet Filter implementation class LogFilter
+ */
+@WebFilter("/*")
+public class LogFilter implements Filter {
+	
+	private Logger logger = Logger.getLogger(LogFilter.class);
+	
+
+    /**
+     * Default constructor. 
+     */
+    public LogFilter() {
+        
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		logger.fatal("치명적인 오류 발생 ");
+		
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
